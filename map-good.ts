085f75e5c90409001f5f72555ff97ff8ce58c1c0
Adrian Połubiński <polubis@wp.pl>
2023-05-04 10:47:06 +0200

    Create map-good.ts

---
+++ map-good.ts
@@ -0,0 +1,13 @@
+// You can change name of function and it's still valid. ✔️
+describe("Transforms one array to another when: ", () => {
+  // No implementation details leak. ✔️
+  it("loops over all items", () => {});
+});
+
+// Purpose of component is described. ✔️
+describe("Allows to work with button when: ", () => {
+  // No API shape leak. ✔️
+  it("uses different motives", () => {});
+  // No API shape leak. ✔️
+  it("handles click", () => {});
+});
