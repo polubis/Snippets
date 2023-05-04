2f6fe051cde945659adba92a5a92111ce538cce1
Adrian Połubiński <polubis@wp.pl>
2023-05-04 11:52:47 +0200

    Update and rename map-full.ts to map-full.test.ts

--- map-full.test.ts
+++ map-full.test.ts
@@ -1,4 +1,4 @@
-describe(`I'm able map any array when: `, () => {
+describe(`I'm able to map any array when: `, () => {
   const items = [1, 2, 3];
 
   it('callback function transforms every item', () => {
