a9e9259c88a67522692ea55c3f7ef4dbe8b57062
Adrian Połubiński <polubis@wp.pl>
2023-05-04 10:11:54 +0200

    Create map-full.ts

---
+++ map-full.ts
@@ -0,0 +1,29 @@
+describe(`I'm able map any array when: `, () => {
+  const items = [1, 2, 3];
+
+  it('callback function transforms every item', () => {
+    const result = map(items, (item) => 'Item: ' + item);
+
+    expect(result).toEqual(['Item: 1', 'Item: 2', 'Item: 3']);
+  });
+
+  it('current item, index and array are passed to callback', () => {
+    const result = map(
+      items,
+      (item, idx, arr) =>
+        `Item: ${item}, index: ${idx}, array length: ${arr.length}`
+    );
+
+    expect(result).toEqual([
+      'Item: 1, index: 0, array length: 3',
+      'Item: 2, index: 1, array length: 3',
+      'Item: 3, index: 2, array length: 3',
+    ]);
+  });
+
+  it('created array have totally new reference', () => {
+    const result = map(items, (item) => item);
+
+    expect(result !== items).toBeTruthy();
+  });
+});
