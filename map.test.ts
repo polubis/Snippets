e46d227ba100af54358a08dc2aed65b4ac1119ec
Adrian Połubiński <polubis@wp.pl>
2023-05-04 09:43:53 +0200

    Create map.test.ts

---
+++ map.test.ts
@@ -0,0 +1,7 @@
+describe(`I'm able map any array when: `, () => {
+  it('callback function transforms every item', () => {});
+
+  it('current item, index and array are passed to callback', () => {});
+
+  it('created array have totally new reference', () => {});
+});
