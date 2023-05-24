acfbd2f736ac2a105a1f4ce61a6c4e725c0d072d
Adrian Połubiński <polubis@wp.pl>
2023-05-24 11:50:21 +0200

    Update map-test-with-spy.test.ts

--- map-test-with-spy.test.ts
+++ map-test-with-spy.test.ts
@@ -4,20 +4,19 @@ describe(`I'm able to map any array when: `, () => {
   const items = [1, 2, 3];
 
   it('calls mapper function to guarantee custom mapping', () => {
-    // Creating spy object and mocking implementation.
-    // Thanks to this we'll be able to listen for calls in expect.
+    // We'll create a spy object and mock implementation.
+    // Thanks to this we'll be able to listen for function calls in expects.
     const mapperSpy = jest.fn().mockImplementation((item) => 'Item: ' + item);
 
-    // Calls function under test and passes spy object.
-    const result = map(items, mapperSpy);
+    map(items, mapperSpy); // We calling function under test.
 
-    // We're checking number of mapperSpy mocked implementation calls.
+    // We'll check number of mapperSpy calls.
     expect(mapperSpy).toHaveBeenCalledTimes(items.length);
-    // First iteration argument pass checked.
+    // We'll check arguments passed in first iteration.
     expect(mapperSpy).toHaveBeenCalledWith(1, 0, items);
-    // Second iteration argument pass checked.
+    // We'll check arguments passed in second iteration.
     expect(mapperSpy).toHaveBeenCalledWith(2, 1, items);
-    // Third iteration argument pass checked.
+    // We'll check arguments passed in third iteration.
     expect(mapperSpy).toHaveBeenCalledWith(3, 2, items);
   });
 });
